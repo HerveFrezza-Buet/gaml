d50847d217ead6818773c42a2a9a7788434af863
Jeremy Fix <jeremy.fix@gmail.com>
2018-03-29 16:54:31 +0200

    upd

--- gaml-datasets/examples/example-002-download.cpp
+++ gaml-datasets/examples/example-002-download.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  auto filename = gaml::datasets::download("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data");
 
-  std::cout << "File downloaded at : " << filename << std::endl;
+  auto dataset = gaml::make_iris_dataset();
+  for(auto& sample: dataset) {
+    for(auto& v: dataset.input_of_data(sample))
+      std::cout << v << " ";
+    std::cout << "-> " << dataset.output_of_data(sample) << std::endl;
+  }
+
+  /*
+  auto diabetes = gaml::datasets::diabetes();
+  unsigned int patient_idx = 0;
+  for(auto& patient: diabetes) {
+    std::cout << "Patient " << patient_idx << " : " << std::endl;
+  }
+  */
 }
--- gaml-datasets/src/gamldatasetsDownload.hpp
+++ gaml-datasets/src/gamldatasetsDownload.hpp
@@ -28,6 +28,8 @@
 #include <string>
 #include <curl/curl.h>
 #include <cstdio>
+#include <array>
+#include <tuple>
 
 #include <experimental/filesystem>
 
@@ -73,6 +75,130 @@ namespace gaml {
 
       return outfilename;
     }
+
+    template<int NB_INPUT,
+	     typename OUTPUT>
+    class CSVParser : public gaml::BasicParser {
+    private:
+      unsigned int skiprows;
+    public:
+      using input_type = std::array<double, NB_INPUT>;
+      using output_type = OUTPUT;
+      using value_type = std::pair<input_type, output_type>;
+
+      
+      CSVParser(unsigned int skiprows): gaml::BasicParser(), skiprows(skiprows) {}
+      
+      void writeBegin(std::ostream& os) const {
+	// Nope
+      }
+      
+      void writeEnd(std::ostream& os) const {
+	// Nope
+      }
+      
+      void writeSeparator(std::ostream& os) const {
+	os << std::endl; // The separation between two data.
+      }
+      
+      void readBegin(std::istream& is) const {
+	// We skip skiprows lines.
+	for(unsigned int i = 0 ; i < skiprows; ++i)
+	  is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      }
+      
+      void readEnd(std::istream& is) const {
+	// Nope
+      }
+      
+      bool readSeparator(std::istream& is) const {
+	// We eat white spaces (as '\n'). 
+	is >> std::ws;
+	
+	// Let us test if more data are available in the file.
+	return !is.eof();
+      }
+      
+      void write(std::ostream& os, const value_type& data) const {
+	for(const auto& v: data.first)
+	  os << v << ", ";
+	os << data.second;
+      }
+      
+      void read(std::istream& is, value_type & data) const {
+	char sep;
+	for(auto& v: data.first)
+	  is >> v >> sep;
+	is >> data.second;
+      }
+    };
+
+
+
+    
+    class Iris {
+    public:
+      using input_type = std::array<double, 4>;
+      using label_type = std::string;
+      using data_type = std::pair<input_type, label_type>;
+      using dataset_type = std::vector<data_type>;
+      
+      using output_type = int;
+      
+    private:
+      dataset_type dataset;
+      
+    public:
+           
+      Iris() {
+	std::string filename = download("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data");
+	
+	CSVParser<4, label_type> parser(0);
+	
+	std::ifstream ifile(filename);
+	auto input_data_stream = gaml::make_input_data_stream(ifile, parser);
+	auto begin = gaml::make_input_data_begin(input_data_stream);
+	auto end = gaml::make_input_data_end(input_data_stream);
+	std::copy(begin, end, std::back_inserter(dataset));
+      }
+
+      auto begin() {
+	return dataset.begin();
+      }
+
+      auto end() {
+	return dataset.end();
+      }
+
+      const input_type& input_of_data(const data_type& data) const {
+	return data.first;
+      }
+
+      output_type output_of_data(const data_type& data) const {
+	if(data.second == std::string("Iris-setosa"))
+	  return 0;
+	else if(data.second == std::string("Iris-versicolor"))
+	  return 1;
+	else
+	  return 2;
+      }
+    };
+  
+    
+    struct Diabetes {
+
+    };
+
+    Diabetes diabetes() {
+      return Diabetes();
+    }
+    
     
   }
+
+  gaml::datasets::Iris make_iris_dataset() {
+    return gaml::datasets::Iris();
+  }
+
+  
 }
