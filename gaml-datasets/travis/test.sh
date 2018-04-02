20770b2a89a9f1471390e4cebdf05965059d79aa
Jeremy Fix <jeremy.fix@gmail.com>
2018-04-02 09:04:59 +0200

    updating travis config

---
+++ gaml-datasets/travis/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# To list the examples :
+# cd gaml-datasets/travis
+# ls -l ../build/examples/example-* | awk '{print $NF}'
+../build/examples/example-001-artificial
+../build/examples/example-002-download
+
+
